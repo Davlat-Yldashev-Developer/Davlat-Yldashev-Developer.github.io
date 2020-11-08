dbe040262fb6d623bdd218af38c4a6bf2fcead6a
Davlat-Yldashev-Developer <73584945+Davlat-Yldashev-Developer@users.noreply.github.com>
2020-11-08 14:04:04 +0300

    work1

---
+++ work1/js/common.js
@@ -0,0 +1,93 @@
+jQuery(($) => {
+    $('.select').on('click', '.select__head', function () {
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $(this).next().fadeOut();
+        } else {
+            $('.select__head').removeClass('open');
+            $('.select__list').fadeOut();
+            $(this).addClass('open');
+            $(this).next().fadeIn();
+        }
+    });
+
+    $('.select').on('click', '.select__item', function () {
+        $('.select__head').removeClass('open');
+        $(this).parent().fadeOut();
+        $(this).parent().prev().text($(this).text());
+        $(this).parent().prev().prev().val($(this).text());
+    });
+
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.select').length) {
+            $('.select__head').removeClass('open');
+            $('.select__list').fadeOut();
+        }
+    });
+});
+
+$(function () {
+  $(document).ready(function() {
+      $('.slider').slick({
+        arrows:true,
+        dots:false,  
+        slidesToShow:1,
+      });
+      $('.min_slider').slick({
+        arrows:true,
+        dots:false,  
+        slidesToShow:7,
+        responsive:[
+      {
+        breakpoint: 1340,
+        settings: {
+          slidesToShow:6,
+        }
+      },{
+        breakpoint: 1080,
+        settings: {
+          slidesToShow:5,
+        }
+      },{
+        breakpoint: 950,
+        settings: {
+          slidesToShow:4,
+        }
+      },{
+        breakpoint: 570,
+        settings: {
+          slidesToShow:3,
+        }
+      },
+    ]
+      });  
+      $('.slider_contact').slick({
+        arrows:true,
+        dots:false,  
+        slidesToShow:4,
+        responsive:[
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow:3,
+        }
+      },{
+        breakpoint: 700,
+        settings: {
+          slidesToShow:2,
+        }
+      },{
+        breakpoint: 450,
+        settings: {
+          slidesToShow:1,
+        }
+      },
+    ]
+      });  
+      $('.section3_content__slider').slick({
+        arrows:true,
+        dots:true,  
+        slidesToShow:1,
+      }); 
+    });
+  });

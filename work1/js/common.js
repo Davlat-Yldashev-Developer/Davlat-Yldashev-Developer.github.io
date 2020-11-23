e1cf3017613e413f6376faef1c9cac5bde096430
Davlat-Yldashev-Developer <73584945+Davlat-Yldashev-Developer@users.noreply.github.com>
2020-11-23 10:13:51 +0300

    аррпро

--- work1/js/common.js
+++ work1/js/common.js
@@ -179,22 +179,6 @@ $(document).on('click','.box_filtr', function(){
     $(this).toggleClass('active')
 });
 
-$('.message_content__left__chat').click(function(event) {
-    $('.message_hover__chat').toggleClass('active');
-    $('body').toggleClass('lock');
-  });
-$('.message_content__left__chat2').click(function(event) {
-    $('.message_hover__chat2').toggleClass('active');
-    $('body').toggleClass('lock');
-  });
-$('.message_content__left__chat3').click(function(event) {
-    $('.message_hover__chat3').toggleClass('active');
-    $('body').toggleClass('lock');
-  });
-$('.message_content__left__chat4').click(function(event) {
-    $('.message_hover__chat4').toggleClass('active');
-    $('body').toggleClass('lock');
-  });
 
 
 $('.mobile__button').click(function(event) {
